99f3a98bc1d9729491a740c4b6c70454eab680f1
niruien <nir@rose-hulman.edu>
2017-11-27 16:57:39 -0500

    mike and rebecca

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -31,29 +31,29 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 import rosegraphics as rg
 window = rg.TurtleWindow()
 mike = rg.SimpleTurtle('turtle')
-mike.pen = rg.Pen('green', 10)
-mike.speed = 5
-size = 150
+mike.pen = rg.Pen('pink', 10)
+mike.speed = 10
+size = 100
 for k in range(10):
     mike.draw_circle(size)
     mike.pen_up()
     mike.right(45)
     mike.right(30)
     mike.pen_down()
-    size = size-10
+
 
 rebecca = rg.SimpleTurtle('turtle')
-rebecca.pen = rg.Pen('pink',10)
+rebecca.pen = rg.Pen('red',10)
 rebecca.speed = 5
-length = 200
-for j in range (15):
-    rebecca.draw_square(length)
+length = 150
+for j in range (5):
+    rebecca.draw_regular_polygon(5,length)
     rebecca.pen_up()
-    rebecca.left(45)
+    rebecca.left(36)
     rebecca.forward(10)
-    rebecca.left(45)
+    rebecca.left(36)
     rebecca.pen_down()
-    length = length-10
+
 
 
 
